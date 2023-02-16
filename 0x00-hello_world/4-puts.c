7f1a88630a6f712797bf1990a4e1ab57b9f5dd86
Kelvin-Mtambo <kelvinmaziko@gmail.com>
2023-02-16 02:15:55 -0800

    4-puts script update 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
-int main(void){	puts("Programming is like building a multilingual puzzle\n");return 0;}
+
+/*
+ * main is the default function
+ * with return type 0 always
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
