138fd798f72c4df609fb0813c87c0ae22e718373
Kelvin-Mtambo <kelvinmaziko@gmail.com>
2023-02-16 03:21:15 -0800

    101-quote.c update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,9 @@
  *
  * Return: Returns a 1 (Success)
  */
-int main() {
+int main(void) 
+{
     const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(2, message, sizeof(message) - 1);
-    return 1;
+    return (1);
 }
