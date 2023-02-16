7d67e2a5a93ba90475e2dc2bbba8ba3f5a3b35b6
Kelvin-Mtambo <kelvinmaziko@gmail.com>
2023-02-16 03:35:44 -0800

    101-quote.c update 5

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Default function
  *
  * Return: Returns a 1 (Success)
  */
-int main(void) 
+int main(void)
 {
-    	const char message[] = "and that piece of art is"+
-		" useful\"- Dora Korpar, 2015-10-19\n";
-    	write(2, message, sizeof(message) - 1);
-    	return (1);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
