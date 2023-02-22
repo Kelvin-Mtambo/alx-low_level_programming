da4e6036402e1204bac99f54fafcd2ec8256f285
Kelvin-Mtambo <kelvinmaziko@gmail.com>
2023-02-22 15:16:33 -0800

    add 2-print_alphabet_x10.c and 2-main.c script

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print _alphabet_x10 - prints alphabet in lower case 10 times
+ * return: 0
+void print_alphabet_x10()
+{
+	char c;
+	int i = 0;
+	while(i <= 9)
+	{
+		_putchar('\n');
+		_putchar('\n');
+		i++;
+	}
+}
