38da0ebaa70d68fed905aaf7666ffc8eb92b6a67
Kelvin-Mtambo <kelvinmaziko@gmail.com>
2023-02-22 14:59:11 -0800

    1-alphabet.c and 1-main.c script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * print_alphabet - prints alphabet in lower cases
+ * Return: returns 0
+ */
+void print_alphabet()
+{
+	char c;
+	for (c = 'a';c <='z';c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
