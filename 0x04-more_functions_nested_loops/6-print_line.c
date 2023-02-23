e89debcf41764be3af53465beb4ea267374c6304
Kelvin-Mtambo <kelvinmaziko@gmail.com>
2023-02-23 04:50:54 -0800

    6-print_line.c script fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,16 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	for (i = 0; i < n; i++)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 1; i <= n; i++)
+		if (n > 0)
 		{
 			_putchar('_');
 		}
-	_putchar('\n');
+		else
+		{
+			_putchar('\n');
+		}
 	}
+	_putchar('\n');
 }
