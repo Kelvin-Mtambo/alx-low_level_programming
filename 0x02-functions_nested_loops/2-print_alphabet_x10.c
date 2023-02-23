44b254f7ca1e5d10fa892968f9daa7f3f950bab4
Kelvin-Mtambo <kelvinmaziko@gmail.com>
2023-02-22 17:43:07 -0800

    2-print_alphabet_x10.c script fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,9 +3,11 @@
 /**
  * print _alphabet_x10 - prints alphabet in lower case 10 times
  * return: 0
-void print_alphabet_x10()
+ */
+void print_alphabet_x10(void)
 {
 	char c;
+
 	int i = 0;
 	while(i <= 9)
 	{
