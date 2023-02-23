f95f4d4170a4d76476488dfa96ace8b44fb9818b
Kelvin-Mtambo <kelvinmaziko@gmail.com>
2023-02-22 18:19:13 -0800

    100-times_table.c fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -14,6 +15,7 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
+
 				if (j == 0)
 				{
 					_putchar(k + '0');
