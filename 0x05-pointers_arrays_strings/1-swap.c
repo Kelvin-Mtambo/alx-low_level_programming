30c2c5fc7419e8ff30f0124048c2e1aea4a14a39
Kelvin-Mtambo <kelvinmaziko@gmail.com>
2023-02-27 14:39:23 -0800

    1-swap.c and 1-main.c scripts

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swap value of two ints
+ * @x: pointer to one int
+ * @y: pointer to second int
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int z;
+
+	z = *x;
+	*x = *y;
+	*y = z;
+}
