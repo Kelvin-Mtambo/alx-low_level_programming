efa717b718acc2798440a3887d7091b88c732426
Kelvin-Mtambo <kelvinmaziko@gmail.com>
2023-03-15 15:30:55 -0700

    1 fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - prints all arguments it receives
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
- */
+ * main - prints the number of arguenments passed to it
+ * @argc: arguemnet count
+ * @argv: array of pointer to arguement strings
+ * Return: 0
+ **/
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-
+/* to suppress unused variable warning */
+	argv = argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
