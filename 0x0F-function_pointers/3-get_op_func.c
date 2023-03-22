5ef42c6a033536f6f72b5925bc7d89a8e879bf44
Kelvin-Mtambo <kelvinmaziko@gmail.com>
2023-03-22 02:21:25 -0700

    task 3 scrits fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,29 @@
-#include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
-
+#include "3-calc.h"
 /**
- * get_op_func - ...
- * @s: ...
+ * get_op_func - get funtion
  *
- * Return: ...
+ * @s: pointer to funtion
+ *
+ * Return: Integer
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div }
-	{ "%", op_mod },
-	{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
 	};
 	int i = 0;
 
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
+		if (*(ops[i]).op == *s)
+			return (ops[i].f);
 		i++;
 	}
-
-	return (0);
+	return (NULL);
 }
